3d7b5a6d25d486205e179f510b7cda9d6c05b41c
Naaman Omar <naamanomar46@gmail.com>
2025-03-18 23:22:17 +0530

    Changed fetch url

--- public/index.js
+++ public/index.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Fetch books from the API and display them
     function fetchBooks() {
-        fetch('https://books-store-g58s.onrender.com')
+        fetch('https://books-store-g58s.onrender.com/books')
             .then(response => response.json())
             .then(data => {
                 booksList.innerHTML = '';  // Clear the list first
