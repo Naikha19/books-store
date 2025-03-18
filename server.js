2aa8ee1860a625541fe984df8d621a6686dcbdf4
Naaman Omar <naamanomar46@gmail.com>
2025-03-18 17:36:28 +0530

    Prepared for Render deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 const path = require("path")
-const port = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Middleware to parse JSON
 app.use(express.json());
